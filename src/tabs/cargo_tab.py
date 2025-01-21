1c57cc8b222013564c1009557ff859efe1da9be8
Sergey Kochemasov <sergey.ko@hotmail.com>
2025-01-21 17:28:31 +0300

    Добавлены и исправлены выпадающие списки на вкладке 'Грузы'

--- src/tabs/cargo_tab.py
+++ src/tabs/cargo_tab.py
@@ -1,4 +1,3 @@
-# src/tabs/cargo_tab.py
 from PyQt5.QtWidgets import QTableWidgetItem, QComboBox, QDateEdit
 from PyQt5.QtCore import QDate
 from src.local_data_manager import LocalDataManager
@@ -15,13 +14,34 @@ class CargoTab:
         # Загрузка данных из базы
         self.cases = self.firebase_manager.get_all_cases() or {}
         self.clients = self.firebase_manager.get_all_clients() or {}
+        self.managers = self.firebase_manager.get_all_managers() or []
+        self.suppliers = self.firebase_manager.get_all_suppliers() or {}
 
-        # Загрузим транспортные компании и пункты назначения
-        self.tk_list = self.data_manager.load_transport_companies() or []
-        self.destination_list = self.data_manager.load_destinations() or []
+        # Настройка вкладки
+        self.setup_cargo_tab()
 
-        # Настройка таблицы "Грузы"
+    def setup_cargo_tab(self):
+        """Настройка вкладки 'Грузы'."""
         self.setup_cargo_table()
+        self.setup_dropdowns()
+
+    def setup_dropdowns(self):
+        """Загрузка данных в выпадающие списки."""
+        # Заполнение выпадающего списка "Номер дела"
+        self.parent.cargoCaseNumberInput.clear()
+        self.parent.cargoCaseNumberInput.addItems(self.cases.keys())
+
+        # Заполнение выпадающего списка "Менеджер"
+        self.parent.cargoManagerInput.clear()
+        self.parent.cargoManagerInput.addItems(self.managers)
+
+        # Заполнение выпадающего списка "Поставщик"
+        self.parent.cargoSupplierInput.clear()
+        self.parent.cargoSupplierInput.addItems(supplier['name'] for supplier in self.suppliers.values())
+
+        # Заполнение выпадающего списка "Клиент"
+        self.parent.cargoCustomerInput.clear()
+        self.parent.cargoCustomerInput.addItems(client['name'] for client in self.clients.values())
 
     def load_cargo_table(self):
         """Метод для загрузки данных в таблицу."""
@@ -45,8 +65,8 @@ class CargoTab:
         self.cargo_table.setItemDelegateForColumn(4, QDateEditDelegate(self.cargo_table))  # Deadline
         self.cargo_table.setItemDelegateForColumn(5, QDateEditDelegate(self.cargo_table))  # ETD
         self.cargo_table.setItemDelegateForColumn(6, QDateEditDelegate(self.cargo_table))  # ETA
-        self.cargo_table.setItemDelegateForColumn(7, ComboBoxDelegate(self.cargo_table, self.tk_list))  # TK
-        self.cargo_table.setItemDelegateForColumn(8, ComboBoxDelegate(self.cargo_table, self.destination_list))  # Destination
+        self.cargo_table.setItemDelegateForColumn(7, ComboBoxDelegate(self.cargo_table, self.data_manager.load_transport_companies()))  # TK
+        self.cargo_table.setItemDelegateForColumn(8, ComboBoxDelegate(self.cargo_table, self.data_manager.load_destinations()))  # Destination
 
     def add_cargo_row(self, row_position):
         """Добавляет строку в таблицу 'Грузы'."""
@@ -70,12 +90,12 @@ class CargoTab:
 
         # TK (Транспортная компания)
         tk_selector = QComboBox()
-        tk_selector.addItems(self.tk_list)
+        tk_selector.addItems(self.data_manager.load_transport_companies())
         self.cargo_table.setCellWidget(row_position, 7, tk_selector)
 
         # Пункт назначения
         destination_selector = QComboBox()
-        destination_selector.addItems(self.destination_list)
+        destination_selector.addItems(self.data_manager.load_destinations())
         self.cargo_table.setCellWidget(row_position, 8, destination_selector)
 
         # Tracking
